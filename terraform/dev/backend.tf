b7cbc9e150c7352233cc6bd491fb0f5a45781704
siva3150 <siva123@gmail.com>
2024-02-05 23:27:57 +0530

    catalogue-deploy

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "sivadevops-state-dev"
+key    = "catalogue"
+region = "us-east-1"
+dynamodb_table = "sivadevops-locking-dev"
---
+++ terraform/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "sivadevops-state-prod"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "sivadevops-locking-prod"
